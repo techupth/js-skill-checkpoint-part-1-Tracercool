b810a13f1ecb51d27fb9b56d404b28898929f021
Mon <tracer.coolkid@gmail.com>
2024-05-22 14:54:12 +0700

    feat:added ex1-5 Skill-checkpoint

--- question-1.js
+++ question-1.js
@@ -6,3 +6,16 @@ const myTodo = [
   { id: 4, todo: "Wash dishes" },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+myTodo.push({id: 5, todo: "Walk the dog"});
+
+const newMyTodo = myTodo.map((item) =>{
+  if (item.id === 4) {
+    return {...item, todo: "Go to the gym"};
+  }
+  return item;
+});
+
+newMyTodo.pop();
+const lastTodo = newMyTodo[newMyTodo.length -1];
+//console.log(NewMyTodo);
+console.log(`To-do id: ${lastTodo.id}, ${lastTodo.todo}`);
--- question-5.js
+++ question-5.js
@@ -18,4 +18,25 @@ const products = [
 ];
 
 const promotionCode = "";
+//const promotionCode = "SALE20";
+//const promotionCode = "SALE50";
 // เริ่มเขียนโค้ดตรงนี้
+function calculateTotalPrice(products, promotionCode) {
+  const totalPrice = products.reduce((total, product) => {
+    return total + (product.price * product.quantity); 
+  }, 0); //คำนวณราคารวมของสินค้า
+
+  //การให้โปรโมชั่น
+  let discount = 0;
+  if (promotionCode === "SALE20") {
+    discount = totalPrice * 0.2;
+  } else if (promotionCode === "SALE50") {
+    discount = totalPrice * 0.5;
+  }
+
+  const updatePrice = totalPrice - discount;
+  return updatePrice;
+}
+
+const totalPrice = calculateTotalPrice(products, promotionCode);
+console.log(totalPrice);
